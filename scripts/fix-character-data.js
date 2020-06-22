fa3ee8021f4bd321f821f270f586d21961e9db20
PhasmaExMachina <cathryne.trallis@gmail.com>
2020-06-21 20:02:46 -0400

    Fix new mod data

--- scripts/fix-character-data.js
+++ scripts/fix-character-data.js
@@ -27,7 +27,7 @@ fs.readdirSync(charactersPath).forEach(charDir => {
     if(characters[code].variants[variant].mods.indexOf(modHash) < 0) {
       characters[code].variants[variant].mods.push(modHash)
     }
-    modHashes.pck[modHash] = modHashes.pck[modHash] || {code, variant, created: Date.now()}
+    modHashes.pck[modHash] = modHashes.pck[modHash] || {code, variant}
     const modPath = path.join(charPath, modHash)
     if(code.match(/^s(c|m)/) && fs.existsSync(path.join(modPath, '00000002'))) {
       fs.renameSync(path.join(modPath, '00000002'), path.join(modPath, 'physics.json'))
@@ -41,6 +41,48 @@ fs.readdirSync(charactersPath).forEach(charDir => {
     modHashes.texture[textureHash] = true
   })
 })
+
+// const findVariant = (hash, character) => {
+//   var variant
+//   Object.keys(character.variants).forEach(vid => {
+//     if(character.variants[vid].mods.indexOf(hash) > -1)
+//       variant = vid
+//   })
+//   return variant
+// }
+
+// const findChild = hash => {
+//   var character
+//   Object.keys(characters).forEach(code => {
+//     Object.keys(characters[code].variants).forEach(variant => {
+//       if(characters[code].variants[variant].mods.indexOf(hash) > -1)
+//         character = characters[code]
+//     })
+//   })
+//   return character
+// }
+
+// Object.keys(modHashes.pck).forEach(hash => {
+//   const character = findChild(hash)
+//   if(character) {
+//     const variant = findVariant(hash, character)
+
+// // Object.keys(modHashes.pck).forEach(hash => {
+// //   const {variant, code} = modHashes.pck[hash] || {}
+// //   if(code) {
+//     // const variant = findVariant(hash, character)
+//     // modHashes.pck[hash] = {
+//     //   code: character.code,
+//     //   variant,
+//     //   created: fs.statSync(path.join(__dirname, '../docs/characters/' + character.code + '_' + variant + '/' + hash))
+//     // }
+//     const stat = fs.statSync(path.join(__dirname, '../docs/characters/' + character.code + '_' + variant + '/' + hash))
+//     const created = new Date(stat.birthtime).getTime()
+//     console.log(modHashes.pck[hash])
+//     modHashes.pck[hash] = typeof modHashes.pck[hash] == 'object' ? modHashes.pck[hash] : {code: character.code, variant, created}
+//     modHashes.pck[hash].created = created
+//   }
+// })
 
 fs.writeFileSync(path.join(__dirname, '../src/data/characters.json'), JSON.stringify(characters, null, 2))
 fs.writeFileSync(path.join(__dirname, '../data/mod-hashes.json'), JSON.stringify(modHashes, null, 2))
