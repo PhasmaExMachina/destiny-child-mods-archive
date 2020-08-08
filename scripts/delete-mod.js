e96aafd9dc429ee086d4fa6da47637f0d32bc76e
PhasmaExMachina <cathryne.trallis@gmail.com>
2020-08-08 08:43:04 -0400

    Delete script

---
+++ scripts/delete-mod.js
@@ -0,0 +1,20 @@
+const fs = require('fs'),
+      path = require('path'),
+      rimraf = require("rimraf"),
+      characters = require('../src/data/characters.json'),
+      mods = require('../src/data/mods.json')
+
+const hash = process.argv[2]
+
+console.log('Deleting', hash)
+
+const mod = mods[hash],
+      character = characters[mod.code],
+      variant = character.variants[mod.variant]
+
+variant.mods.splice(variant.mods.indexOf(hash), 1)
+delete mods[hash]
+
+rimraf.sync(path.join(__dirname, '../docs/characters/' + mod.code + '_' + mod.variant + '/' + hash))
+fs.writeFileSync(path.resolve(__dirname, '../src/data/mods.json'), JSON.stringify(mods, null, 2))
+fs.writeFileSync(path.resolve(__dirname, '../src/data/characters.json'), JSON.stringify(characters, null, 2))
