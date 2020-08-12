beea1822e807afa32f352968e3f46042b2e838a6
PhasmaExMachina <cathryne.trallis@gmail.com>
2020-08-11 23:30:01 -0400

    Remove lots of duplicates

--- scripts/delete-mod.js
+++ scripts/delete-mod.js
@@ -19,11 +19,11 @@ delete mods[hash]
 Object.keys(modders).forEach(modder => {
   if(modders[modder].mods.indexOf(hash) > -1)
     modders[modder].mods.splice(modders[modder].mods.indexOf(hash), 1)
-  if(modders[modder].modsUsingAssets.indexOf(hash) > -1)
+  if(modders[modder].modsUsingAssets && modders[modder].modsUsingAssets.indexOf(hash) > -1)
     modders[modder].modsUsingAssets.splice(modders[modder].modsUsingAssets.indexOf(hash), 1)
 })
 
-rimraf.sync(path.join(__dirname, '../docs/characters/' + mod.code + '_' + mod.variant + '/' + hash))
 fs.writeFileSync(path.resolve(__dirname, '../src/data/mods.json'), JSON.stringify(mods, null, 2))
 fs.writeFileSync(path.resolve(__dirname, '../src/data/characters.json'), JSON.stringify(characters, null, 2))
 fs.writeFileSync(path.resolve(__dirname, '../src/data/modders.json'), JSON.stringify(modders, null, 2))
+rimraf.sync(path.join(__dirname, '../docs/characters/' + mod.code + '_' + mod.variant + '/' + hash))
